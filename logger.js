bc2375d9f8954cd6af5e5afdc237af3a9f18e53c
adam20maidany <adam20maidany@gmail.com>
2025-05-29 18:31:47 -0700

    Update logger.js

--- logger.js
+++ logger.js
@@ -1,19 +1,13 @@
-const winston = require('winston');
-
-const logger = winston.createLogger({
-  level: 'info',
-  format: winston.format.combine(
-    winston.format.timestamp(),
-    winston.format.simple()
-  ),
-  transports: [
-    new winston.transports.Console({
-      format: winston.format.combine(
-        winston.format.colorize(),
-        winston.format.simple()
-      )
-    })
-  ]
-});
-
-module.exports = logger;
+// Simple logger utility
+function log(level, message, data = null) {
+    const timestamp = new Date().toISOString();
+    const logEntry = data ? `${timestamp} [${level}] ${message} ${JSON.stringify(data)}` : `${timestamp} [${level}] ${message}`;
+    console.log(logEntry);
+  }
+  
+  module.exports = {
+    info: (message, data) => log('INFO', message, data),
+    error: (message, data) => log('ERROR', message, data),
+    warn: (message, data) => log('WARN', message, data),
+    debug: (message, data) => log('DEBUG', message, data)
+  };
